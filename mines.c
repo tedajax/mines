7e48ea3aabe5df72580a88c9e7c9aa547a5cd9ae
Ted Dobyns <ted@tedajax.net>
2014-01-13 23:01:24 -0800

    click boxes and they turn red

--- mines.c
+++ mines.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <time.h>
+#include <math.h>
 
 #include <SDL2/SDL.h>
 
@@ -24,7 +25,8 @@ bool init();
 bool load_content();
 void cleanup();
 
-int32_t xy_to_index(int32_t x, int32_t y);
+int32_t xy_to_index(int32_t x, int32_t y, int32_t w, int32_t h);
+void index_to_xy(int32_t index, int32_t w, int32_t h, int32_t *x, int32_t *y);
 
 typedef struct mineblock_t {
 	bool safe;
@@ -34,10 +36,15 @@ typedef struct mineblock_t {
 } mineblock_t;
 
 typedef struct minefield_t {
+	int32_t width, height, size;
 	mineblock_t *blocks;
 } minefield_t;
 
-void minefield_generate(int32_t w, int32_t h, int32_t mineCount, minefield_t *minefield);
+void minefield_reset(minefield_t *self, int32_t w, int32_t h);
+void minefield_seed_mines(minefield_t *self, int32_t mineCount,
+	int32_t safeX, int32_t safeY);
+bool minefield_reveal(minefield_t *self, int32_t x, int32_t y);
+int32_t minefield_get_adjacent_count(minefield_t *self, int32_t x, int32_t y);
 bool minefield_position_valid(minefield_t *self, int32_t x, int32_t y);
 mineblock_t *minefield_block(minefield_t *self, int32_t x, int32_t y);
 
@@ -45,6 +52,9 @@ typedef struct mouse_t {
 	int32_t x;
 	int32_t y;
 	bool clickDown;
+
+	int32_t selectedX;
+	int32_t selectedY;
 } mouse_t;
 
 mouse_t g_mouse;
@@ -64,8 +74,12 @@ typedef enum {
 
 typedef struct button_t {
 	bool enabled;
+	int32_t gx;
+	int32_t gy;
 	button_state_e state;
 	SDL_Rect rectangle;
+
+	mineblock_t *data;
 } button_t;
 
 button_t *buttons;
@@ -74,7 +88,7 @@ button_t *button_new(int32_t x, int32_t y, int32_t w, int32_t h);
 void button_reset(int32_t x, int32_t y, int32_t w, int32_t h, button_t *dest);
 void button_render(button_t *self);
 bool button_contains(button_t *self, int32_t x, int32_t y);
-void button_update_state(button_t *self, mouse_t mouse);
+void button_update_state(button_t *self, mouse_t *mouse);
 
 int main(int argc, char *argv[]) {
 	if (!init()) {
@@ -84,6 +98,9 @@ int main(int argc, char *argv[]) {
 	if (!load_content()) {
 		return 1;
 	}
+
+	minefield_t minefield;
+	minefield_reset(&minefield, MINEFIELD_WIDTH, MINEFIELD_HEIGHT);
 
 	buttons = (button_t *)malloc(sizeof(button_t) * MINEFIELD_COUNT);
 
@@ -95,17 +112,18 @@ int main(int argc, char *argv[]) {
 				j * 32,
 				BUTTON_SIZE,
 				BUTTON_SIZE,
-				&buttons[b++]
+				&buttons[b]
 			);
+			buttons[b].gx = i;
+			buttons[b].gy = j;
+			buttons[b].data = &minefield.blocks[b];
+			++b;
 		}
 	}
 
 	g_mouse.x = 0;
 	g_mouse.y = 0;
 	g_mouse.clickDown = false;
-
-	minefield_t minefield;
-	minefield_generate(MINEFIELD_WIDTH, MINEFIELD_HEIGHT, 10, &minefield);
 
 	bool running = true;
 	SDL_Event sdlEvent;
@@ -142,6 +160,16 @@ int main(int argc, char *argv[]) {
 
 					if (sdlEvent.button.button == SDL_BUTTON_LEFT) {
 						g_mouse.clickDown = false;
+
+						printf("%d, %d - %d %d\n", 
+							g_mouse.x,
+							g_mouse.y,
+							g_mouse.selectedX,
+							g_mouse.selectedY);
+
+						minefield_reveal(&minefield, 
+							g_mouse.selectedX,
+							g_mouse.selectedY);
 					}
 					break;
 			}
@@ -156,7 +184,7 @@ int main(int argc, char *argv[]) {
 		SDL_RenderClear(g_renderer);
 
 		for (int button = 0; button < MINEFIELD_COUNT; ++button) {
-			button_update_state(&buttons[button], g_mouse);
+			button_update_state(&buttons[button], &g_mouse);
 			button_render(&buttons[button]);
 		}		
 
@@ -212,30 +240,100 @@ void cleanup() {
 	SDL_Quit();
 }
 
-int32_t xy_to_index(int32_t x, int32_t y) {
-	return y * MINEFIELD_WIDTH + x;
+int32_t xy_to_index(int32_t x, int32_t y, int32_t w, int32_t h) {
+	if (x < 0 || y < 0 || x >= w || y >= h) {
+		return -1;
+	}
+	return x * h + y;
 }
 
-void minefield_generate(int32_t w, int32_t h, int32_t mineCount, minefield_t *minefield) {
-	minefield->blocks = (mineblock_t *)malloc(sizeof(mineblock_t) * (w * h));
-
-	//init blocks
-	for (int32_t b = 0; b < MINEFIELD_COUNT; ++b) {
-		minefield->blocks[b].safe = true;
-		minefield->blocks[b].flagged = false;
-		minefield->blocks[b].opened = false;
-		minefield->blocks[b].adjacent_mines = 0;
+void index_to_xy(int32_t index, int32_t w, int32_t h, int32_t *x, int32_t *y) {
+	if (index < 0 || index >= (w * h)) {
+		*x = -1;
+		*y = -1;
+		return;
 	}
 
+	*x = index % w;
+	*y = floor(index / w);
+}
+
+void minefield_reset(minefield_t *self, int32_t w, int32_t h) {
+	self->width = w;
+	self->height = h;
+	self->size = w * h;
+	self->blocks = (mineblock_t *)malloc(sizeof(mineblock_t) *
+										 self->size);
+
+	//init blocks
+	for (int32_t b = 0; b < self->size; ++b) {
+		self->blocks[b].safe = true;
+		self->blocks[b].flagged = false;
+		self->blocks[b].opened = false;
+		self->blocks[b].adjacent_mines = 0;
+	}
+}
+
+void minefield_seed_mines(minefield_t *self, int32_t mineCount,
+	int32_t safeX, int32_t safeY) {
 	while (mineCount > 0) {
-		int32_t rx = rand() % MINEFIELD_WIDTH;
-		int32_t ry = rand() % MINEFIELD_HEIGHT;
-		int32_t r = xy_to_index(rx, ry);
-		if (minefield->blocks[r].safe) {
-			minefield->blocks[r].safe = false;
+		int32_t rx = rand() % self->width;
+		int32_t ry = rand() % self->height;
+
+		if (rx == safeX && ry == safeY) {
+			continue;
+		}
+
+		int32_t r = xy_to_index(rx, ry, self->width, self->height);
+		if (self->blocks[r].safe) {
+			self->blocks[r].safe = false;
 			--mineCount;
 		}
 	}
+}
+
+bool minefield_reveal(minefield_t *self, int32_t x, int32_t y) {
+	if (!minefield_position_valid(self, x, y)) {
+		return true;
+	}
+
+	int32_t index = xy_to_index(x, y, self->width, self->height);
+	if (self->blocks[index].safe) {
+		self->blocks[index].opened = true;
+		self->blocks[index].adjacent_mines = minefield_get_adjacent_count(self,
+			x,
+			y);
+	}
+}
+
+int32_t minefield_get_adjacent_count(minefield_t *self, int32_t x, int32_t y) {
+	int32_t tlx = x - 1;
+	int32_t tly = y - 1;
+
+	int32_t count = 0;
+
+	int32_t indices[8];
+	indices[0] = xy_to_index(tlx + 0, tly + 0, self->width, self->height);
+	indices[1] = xy_to_index(tlx + 1, tly + 0, self->width, self->height);
+	indices[2] = xy_to_index(tlx + 2, tly + 0, self->width, self->height);
+
+	indices[3] = xy_to_index(tlx + 0, tly + 1, self->width, self->height);
+	indices[4] = xy_to_index(tlx + 2, tly + 1, self->width, self->height);
+
+	indices[5] = xy_to_index(tlx + 0, tly + 2, self->width, self->height);
+	indices[6] = xy_to_index(tlx + 1, tly + 2, self->width, self->height);
+	indices[7] = xy_to_index(tlx + 2, tly + 2, self->width, self->height);
+
+	for (int32_t i = 0; i < 8; ++i) {
+		int32_t index = indices[i];
+		if (index >= 0) {
+			if (!self->blocks[index].safe) {
+				++count;
+			}
+		}
+	}
+
+	return count;
 }
 
 bool minefield_position_valid(minefield_t *self, int32_t x, int32_t y) {
@@ -248,7 +346,7 @@ mineblock_t *minefield_block(minefield_t *self, int32_t x, int32_t y) {
 		return NULL;
 	}
 
-	return &self->blocks[xy_to_index(x, y)];
+	return &self->blocks[xy_to_index(x, y, self->width, self->height)];
 }
 
 button_t *button_new(int32_t x, int32_t y, int32_t w, int32_t h) {
@@ -277,25 +375,29 @@ void button_render(button_t *self) {
 		0, 0, 0, 255
 	};
 
-	switch (self->state) {
-		default:
-		case STATE_UP:
-			color.r = 200;
-			color.g = 200;
-			color.b = 200;
-			break;
+	if (!self->data->opened) {
+		switch (self->state) {
+			default:
+			case STATE_UP:
+				color.r = 200;
+				color.g = 200;
+				color.b = 200;
+				break;
 
-		case STATE_DOWN:
-			color.r = 50;
-			color.g = 50;
-			color.b = 50;
-			break;
+			case STATE_DOWN:
+				color.r = 50;
+				color.g = 50;
+				color.b = 50;
+				break;
 
-		case STATE_HOVER:
-			color.r = 125;
-			color.g = 125;
-			color.b = 125;
-			break;
+			case STATE_HOVER:
+				color.r = 125;
+				color.g = 125;
+				color.b = 125;
+				break;
+		}
+	} else {
+		color.r = 255;
 	}
 
 	SDL_SetRenderDrawColor(g_renderer, color.r, color.g, color.b, color.a);
@@ -309,11 +411,13 @@ bool button_contains(button_t *self, int32_t x, int32_t y) {
 			 y > self->rectangle.y + self->rectangle.h);
 }
 
-void button_update_state(button_t *self, mouse_t mouse) {
-	if (!button_contains(self, mouse.x, mouse.y)) {
+void button_update_state(button_t *self, mouse_t *mouse) {
+	if (!button_contains(self, mouse->x, mouse->y)) {
 		self->state = STATE_UP;
 	} else {
-		if (mouse.clickDown) {
+		if (mouse->clickDown) {
+			mouse->selectedX = self->gx;
+			mouse->selectedY = self->gy;
 			self->state = STATE_DOWN;
 		} else {
 			self->state = STATE_HOVER;
